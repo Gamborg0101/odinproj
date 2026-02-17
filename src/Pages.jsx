3a267d995dc967eaa971dc44d528a98b9512c2b1
Casper Gamborg <Gamborgc@gmail.com>
2026-02-17 13:58:09 +0100

    Form data is correctly being used. Next up is setting up the cv-form

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -4,26 +4,107 @@ import PageHeader from "./components/PageHeader";
 import { useState } from "react";
 
 export default function Pages() {
-  const [formData, setFormData] = useState();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    eduExperience: "",
+    eduYear: "",
+    eduDescription: "",
+    pracExperience: "",
+    pracYear: "",
+    pracDescription: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    console.log(formData);
+  }
 
   return (
     <div className="grid grid-cols-2">
       <form action="">
         <PageHeader title="Contact information" />
-        <Page type="text" placeholder="First name" />
-        <Page type="text" placeholder="Last name" />
-        <Page type="email" placeholder="Email" />
-        <Page type="tel" placeholder="Phone" />
+        <Page
+          name="firstName"
+          type="text"
+          placeholder="First name"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+        <Page
+          name="lastName"
+          type="text"
+          placeholder="Last name"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <Page
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <Page
+          name="phone"
+          type="tel"
+          placeholder="Phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
         <Button title="Insert" />
         <PageHeader title="Experiences" />
-        <Page type="text" placeholder="Education / Course" />
-        <Page type="text" placeholder="Year" />
-        <Page type="email" placeholder="Small description" />
+        <Page
+          name="eduExperience"
+          type="text"
+          placeholder="Education / Course"
+          value={formData.eduExperience}
+          onChange={handleChange}
+        />
+        <Page
+          name="eduYear"
+          type="text"
+          placeholder="Year"
+          value={formData.eduYear}
+          onChange={handleChange}
+        />
+        <Page
+          name="eduDescription"
+          type="text"
+          placeholder="Small description"
+          value={formData.eduDescription}
+          onChange={handleChange}
+        />
         <Button title="Insert" />
         <PageHeader title="Pratical" />
-        <Page type="text" placeholder="Experience" />
-        <Page type="text" placeholder="Year" />
-        <Page type="email" placeholder="Small description" />
+        <Page
+          name="pracExperience"
+          type="text"
+          placeholder="Experience"
+          value={formData.pracExperience}
+          onChange={handleChange}
+        />
+        <Page
+          name="pracYear"
+          type="text"
+          placeholder="Year"
+          value={formData.pracYear}
+          onChange={handleChange}
+        />
+        <Page
+          name="pracDescription"
+          type="text"
+          placeholder="Small description"
+          value={formData.pracExperience}
+          onChange={handleChange}
+        />
         <Button title="Insert" />
       </form>
     </div>
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,14 @@
-export default function Page({ placeholder, type }) {
+export default function Page({ name, placeholder, type, value, onChange }) {
   return (
     <div className="w-full">
       <input
+        name={name}
         type={type}
         id={placeholder}
         placeholder={placeholder}
+        value={value}
         className="w-full p-4 bg-blue-100 rounded-2xl"
+        onChange={onChange}
       />
       <label htmlFor={placeholder} />
     </div>
